ef116d64033797911c4e8d81e1fac43e9859e6a1
Michael Schreier <mjschreier1@gmail.com>
2018-08-04 16:54:30 -0600

    implemented header skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { LinksComponent } from './components/header/links/links.component';
+import { SocialIconsComponent } from './components/header/social-icons/social-icons.component';
+import { HamburgerComponent } from './components/header/hamburger/hamburger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LinksComponent,
+    SocialIconsComponent,
+    HamburgerComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
